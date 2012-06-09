006beed4b409c1cace7115e6921602e19a6228d4
rhysr <rhysr@mega-city-one.co.uk>
2012-06-09 13:24:15 +0100

    php settings module of zend framework 2

---
+++ Module.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace ZF2PhpSettings;
+
+use Zend\EventManager\EventInterface as Event;
+
+class Module
+{
+    public function getConfig()
+    {
+        return include __DIR__ . '/config/module.config.php';
+    }
+
+    public function onBootstrap(Event $e)
+    {
+        $config = $e->getParam('application')->getConfiguration();
+        if (!isset($config['phpSettings'])) {
+            return;
+        }
+
+        $phpSettings = $config['phpSettings'];
+        foreach ($phpSettings as $setting => $value) {
+
+            if (false === ini_set($setting, $value)) {
+                throw new \RuntimeException('Cannot set ini \'' . $setting . '\' to \'' . $value);
+            }
+        }
+    }
+}
